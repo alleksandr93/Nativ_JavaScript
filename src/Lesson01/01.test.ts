39434c927f39c86df7dbe829d21eb3b590bf0500
Aleksandr <aleksandr-ishutin93@mail.ru>
2024-05-07 03:59:18 +0400

    first commit

---
+++ src/Lesson01/01.test.ts
@@ -0,0 +1,12 @@
+
+const sentens = 'Hello my friends'
+
+function sum(a:number,b:number) {
+    return a+b
+}
+function mult(a:number,b:number) {
+    return a*b
+}
+function splitIntoWorlds(sentens:string){
+    return ['hello','my','friend']
+}
